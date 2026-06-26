f87058cf7711fd008e48389a4fa24e3d3abac367
burlin_gauna <burlin_gauna@yahoo.com>
2026-06-26 13:06:22 +0800

    imported initial classes

---
+++ Source/PXII/Public/Utility/PxiiCombatBPLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PxiiCombatBPLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PXII_API UPxiiCombatBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PXII/Public/Utility/PxiiMathBPLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PxiiMathBPLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PXII_API UPxiiMathBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
