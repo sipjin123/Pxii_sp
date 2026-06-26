efd8462e421b36e3623d588a7f8b6e56ec30464d
burlin_gauna <burlin_gauna@yahoo.com>
2026-06-26 11:42:58 +0800

    setup initial files part1

---
+++ Source/PXII/PXII.Build.cs
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class PXII : ModuleRules
+{
+	public PXII(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"NavigationSystem",
+			"AIModule",
+			"GameplayTasks",
+			"UMG",
+			"Slate",
+			"SlateCore",
+			"EnhancedInput",
+			"Niagara",
+			"PhysicsCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
