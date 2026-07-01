0578ccfe08b97f343f7df788024656c63df3cd06
burlin_gauna <burlin_gauna@yahoo.com>
2026-07-01 17:48:27 +0800

    burlin - initial setup dodge trigger

---
+++ Source/PXII/Public/GAS/Action/PxiiGA_Dodge.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PxiiGameplayAbilityBase.h"
+#include "PxiiGA_Dodge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PXII_API UPxiiGA_Dodge : public UPxiiGameplayAbilityBase
+{
+	GENERATED_BODY()
+	
+};
