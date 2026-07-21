243148d9afcce8516eb48aaf1ecf69b73a550263
burlin_gauna <burlin_gauna@yahoo.com>
2026-07-21 14:29:05 +0800

    burlin - added base class and bp for yin yang cost

---
+++ Source/PXII/Public/GAS/PxiiGameplayEffectCost.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PxiiGameplayEffectBase.h"
+#include "PxiiGameplayEffectCost.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PXII_API UPxiiGameplayEffectCost : public UPxiiGameplayEffectBase
+{
+	GENERATED_BODY()
+	
+};
