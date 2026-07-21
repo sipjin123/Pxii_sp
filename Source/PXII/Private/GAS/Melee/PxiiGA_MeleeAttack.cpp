f33e696c39cc66bb90ef5ab10ba18d1ebae7119c
burlin_gauna <burlin_gauna@yahoo.com>
2026-07-21 11:49:16 +0800

    updated combo system base class

--- Source/PXII/Private/GAS/Melee/PxiiGA_MeleeAttack.cpp
+++ Source/PXII/Private/GAS/Melee/PxiiGA_MeleeAttack.cpp
@@ -2,4 +2,73 @@
 
 
 #include "GAS/Melee/PxiiGA_MeleeAttack.h"
+#include "Character/PxiiCharacter.h"
 
+bool UPxiiGA_MeleeAttack::CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                             const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags,
+                                             const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+	/*
+	//auto* Character = CastChecked<APxiiCharacter>(ActorInfo->AvatarActor.Get());
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		
+		UE_LOG(LogTemp, Warning, TEXT("MeleeAttackCPP X"));
+		return false;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("MeleeAttackCPP Y"));
+	return true;*/
+}
+
+void UPxiiGA_MeleeAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* OwnerInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	/*
+	if (!CommitAbility(Handle, OwnerInfo, ActivationInfo))
+	{
+		return;
+	}*/
+	
+	Super::ActivateAbility(Handle, OwnerInfo, ActivationInfo, TriggerEventData);
+}
+
+void UPxiiGA_MeleeAttack::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+#pragma region Combat
+
+UPxiiGA_MeleeAttack::UPxiiGA_MeleeAttack(const FObjectInitializer& ObjectInitializer)
+{
+	
+}
+
+void UPxiiGA_MeleeAttack::OnCompleted()
+{
+	K2_OnCompleted();
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UPxiiGA_MeleeAttack::OnBlendOut()
+{
+	K2_OnBlendOut();
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UPxiiGA_MeleeAttack::OnInterrupted()
+{
+	K2_OnInterrupted();
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+void UPxiiGA_MeleeAttack::OnCancelled()
+{
+	K2_OnCancelled();
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+#pragma endregion
--- Source/PXII/Public/GAS/Melee/PxiiGA_MeleeAttack.h
+++ Source/PXII/Public/GAS/Melee/PxiiGA_MeleeAttack.h
@@ -13,5 +13,42 @@ UCLASS()
 class PXII_API UPxiiGA_MeleeAttack : public UPxiiGA_OneshotBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	explicit UPxiiGA_MeleeAttack(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION()
+	virtual void OnCompleted();
+
+	UFUNCTION()
+	virtual void OnBlendOut();
+
+	UFUNCTION()
+	virtual void OnInterrupted();
+
+	UFUNCTION()
+	virtual void OnCancelled();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "PXII|Ability|Dodge", DisplayName = "OnCompleted", meta = (ScriptName = "OnCompleted"))
+	void K2_OnCompleted();							   
+													   
+	UFUNCTION(BlueprintImplementableEvent, Category = "PXII|Ability|Dodge", DisplayName = "OnBlendOut", meta = (ScriptName = "OnBlendOut"))
+	void K2_OnBlendOut();							   
+													   
+	UFUNCTION(BlueprintImplementableEvent, Category = "PXII|Ability|Dodge", DisplayName = "OnInterrupted", meta = (ScriptName = "OnInterrupted"))
+	void K2_OnInterrupted();						   
+													   
+	UFUNCTION(BlueprintImplementableEvent, Category = "PXII|Ability|Dodge", DisplayName = "OnCancelled", meta = (ScriptName = "OnCancelled"))
+	void K2_OnCancelled();
+
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* OwnerInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<UAnimMontage> MontageToPlay;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Gameplay Effects")
+	TSubclassOf<UGameplayEffect> GE_ComboWindow;
 };
