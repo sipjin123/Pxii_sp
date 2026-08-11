33609269e5ceaf4702435ce1ff59371ebed89d11
burlin_gauna <burlin_gauna@yahoo.com>
2026-08-11 18:52:33 +0800

    initial setup infliction base trigger

---
+++ Source/PXII/Public/Interface/PxiiDamageableInterface.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "UObject/Object.h"
+#include "PxiiDamageableInterface.generated.h"
+
+/**
+ * 
+ */
+
+UINTERFACE(MinimalAPI, BlueprintType)
+class UPxiiDamageableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class PXII_API IPxiiDamageableInterface
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintNativeEvent,BlueprintCallable,Category="Combat")
+	APawn* GetPawnOwner();
+
+};
