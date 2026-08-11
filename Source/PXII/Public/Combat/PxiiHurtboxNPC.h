121b059e46a659d36ad1faf6f4327f2cb90aa440
burlin_gauna <burlin_gauna@yahoo.com>
2026-08-11 12:36:34 +0800

    created wip enemy alpha and body parts

---
+++ Source/PXII/Public/Combat/PxiiHurtboxNPC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Combat/PxiiHurtboxBase.h"
+#include "PxiiHurtboxNPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PXII_API APxiiHurtboxNPC : public APxiiHurtboxBase
+{
+	GENERATED_BODY()
+	
+};
