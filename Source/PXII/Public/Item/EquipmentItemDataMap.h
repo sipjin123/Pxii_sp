7615414d26532ebb49eac0a997d162bc2a4f1694
burlin_gauna <burlin_gauna@yahoo.com>
2026-09-04 12:50:28 +0800

    fetch from main

---
+++ Source/PXII/Public/Item/EquipmentItemDataMap.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "EquipmentSlot.h"
+#include "ItemDataMap.h"
+#include "Engine/DataAsset.h"
+#include "EquipmentItemDataMap.generated.h"
+
+UCLASS(BlueprintType)
+class PXII_API UEquipmentItemDataCollection : public UBaseItemDataCollection
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<TObjectPtr<UEquipmentItemData>> Data;
+};
+
+USTRUCT(BlueprintType)
+struct FEquipmentSlotMap
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	EEquipmentSlot SlotType;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UEquipmentItemDataCollection> Collection;
+};
+
+UCLASS(BlueprintType)
+class PXII_API UEquipmentItemDataMap : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FEquipmentSlotMap> Map;
+};
---
+++ Source/PXII/Public/Item/ItemDataMap.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "BaseItemData.h"
+#include "Engine/DataAsset.h"
+#include "ItemDataMap.generated.h"
+
+UCLASS(BlueprintType)
+class PXII_API UBaseItemDataCollection : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+};
+
+
+UCLASS(BlueprintType)
+class PXII_API UItemDataCollection : public UBaseItemDataCollection
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<TObjectPtr<UBaseItemData>> Data;
+};
+
+UCLASS(BlueprintType)
+class PXII_API UItemDataMap : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<TObjectPtr<UBaseItemDataCollection>> ItemMap;
+};
